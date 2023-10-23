4eb0a8338a94402926ee7cd079154171cae3c5f3
Renan J Ortega <renanortega82@gmail.com>
2023-10-23 14:08:55 -0300

    refactor: separate logic in other files.

--- src/app.js
+++ src/app.js
@@ -1,31 +1,8 @@
 import 'dotenv/config'
 import path from 'path';
 import fs from 'fs';
-import { fileURLToPath } from 'url';
-import { 
-    Client, 
-    IntentsBitField, 
-    Collection 
-} from 'discord.js';
-import { LoadCommands } from './commands.js';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-const client = new Client({
-    intents: [
-        IntentsBitField.Flags.Guilds,
-        IntentsBitField.Flags.GuildMembers,
-        IntentsBitField.Flags.GuildMessages,
-        IntentsBitField.Flags.MessageContent,
-    ],
-});
-
-client.commands = new Collection();
-const commands = LoadCommands();
-for (const command of commands) {
-    client.commands.set(command.data.name, command);
-}
+import __dirname from './utils/dirname.js'
+import Client from './client.js';
 
 const eventsPath = path.join(__dirname, 'events');
 const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
@@ -36,10 +13,10 @@ for (const file of eventFiles) {
     const event = eventModule.default;
 
     if (event.once) {
-        client.once(event.name, (...args) => event.execute(...args));
+        Client.once(event.name, (...args) => event.execute(...args));
     } else {
-        client.on(event.name, (...args) => event.execute(...args));
+        Client.on(event.name, (...args) => event.execute(...args));
     }
 }
 
-await client.login(process.env.DISCORD_TOKEN)
+await Client.login(process.env.DISCORD_TOKEN)
