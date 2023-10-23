7d2259508a258e1f643df78da455d1dd7b247484
Renan J Ortega <renanortega82@gmail.com>
2023-10-23 17:48:05 -0300

    style: return list.

--- src/commands/index.js
+++ src/commands/index.js
@@ -1,6 +1,9 @@
 import PING_COMMAND from "./fun/ping.js";
+import ANALYZE_COMMAND from "./food/analyze.js";
 
 export function GetCommands(){
-    const commands = [PING_COMMAND];
-    return commands;
+    return [
+        PING_COMMAND, 
+        ANALYZE_COMMAND
+    ];
 }
