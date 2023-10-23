cacaf694731ce6279622fb10e0e563d733dca10e
Renan J Ortega <renanortega82@gmail.com>
2023-10-23 13:35:27 -0300

    style: adjust idents.

--- src/commands/fun/ping.js
+++ src/commands/fun/ping.js
@@ -1,8 +1,8 @@
 import { SlashCommandBuilder } from 'discord.js';
 
 const data = new SlashCommandBuilder()
-.setName('ping')
-.setDescription('Reply with pong!');
+    .setName('ping')
+    .setDescription('Reply with pong!');
 
 const execute = async(interaction) => {
     await interaction.reply('Pong!');
