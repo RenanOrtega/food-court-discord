b97a58c6b38bf8e5c80938ca58d9c86002ea0c56
Renan J Ortega <renanortega82@gmail.com>
2023-10-24 02:45:15 -0300

    feat: add foods sync.

--- src/events/ready.js
+++ src/events/ready.js
@@ -1,9 +1,11 @@
 import { Events } from "discord.js";
+import { Foods } from "../sequelize/models/foods.js";
 
 export default {
     name: Events.ClientReady,
     once: true,
     execute(client) {
+        Foods.sync();
         console.log(`✔ ${client.user.tag} is online.`)
     }
 }
